d36658a0680168841a7367972597781b788d9548
nevillionaire <nevillepeter12@gmail.com>
2022-05-31 13:19:59 +0300

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for m in range(97, 123):
-    if (m != 101 and m != 113):
-        print("{}".format(chr(m)), end='')
+for i in range(97, 123):
+    if i == 101 or i == 113:
+        continue
+    print("{:c}".format(i), end='')
