110a95a8b33290dde7670d7e5bd32124e0ad7d3b
nevillionaire <nevillepeter12@gmail.com>
2022-06-29 23:16:52 +0300

    789

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""String manipulations"""
+"""
+A function that prints My name is <first name> <last name>
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints a string.
-    Validates string inputs, the display
-    the full name
-
-    """
-    if type(first_name) != str:
-        raise TypeError("first name must be a string")
+    """function that print complete name"""
+    if type(first_name) != str or first_name == "":
+        raise TypeError("first_name must be a string")
     if type(last_name) != str:
-        raise TypeError("last name must be a string")
+        raise TypeError("last_name must be a string")
+    """print(f"{My name is {first_name} {last_name}")"""
     print("My name is {} {}".format(first_name, last_name))
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
-"""Text indentations"""
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    """Prints a text with 2 new lines
+    """Print text with two new lines after each '.', '?', and ':'.
+
     Args:
         text (string): The text to print.
     Raises:
         TypeError: If text is not a string.
-
     """
-    special = ['.', '?', ':']
-    if type(text) != str:
-        raise TypeError("text must be string")
-    for x in text:
-        print(x, end='')
-        if x in special:
-            print('\n\n', end='')
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
